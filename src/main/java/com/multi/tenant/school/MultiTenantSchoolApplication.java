26ff705fd470121322159e17af6dc428a6d44662
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2025-04-14 07:45:04 -0300

    setup projeto

---
+++ src/main/java/com/multi/tenant/school/MultiTenantSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.multi.tenant.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantSchoolApplication.class, args);
+	}
+
+}
